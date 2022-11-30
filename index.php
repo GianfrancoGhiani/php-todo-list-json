d680181a61c0ca4d7ff4986698e90c7122c5d55a
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-11-30 16:06:34 +0100

    scaffolder

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="script.js" defer></script>
+    <link rel="stylesheet" href="style/style.css">
+    <title>ToDo List</title>
+</head>
+
+<body>
+
+
+    <div id="app" class="container">
+        <form method="POST">
+
+
+        </form>
+
+
+    </div>
+
+
+
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,43 @@
+/*
+Dobbiamo creare una web-app che permetta di leggere e scrivere una lista di Todo.
+Deve essere anche gestita la persistenza dei dati leggendoli da, e scrivendoli in un file JSON.
+
+Stack
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
+
+Consigli
+Nello svolgere l’esercizio seguite un approccio graduale.
+Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”).
+Lo step successivo è quello di “testare" l'invio di un nuovo task, sapendo che manca comunque la persistenza lato server 
+(ancora non memorizzate i dati da nessuna parte).
+Solo a questo punto sarà utile passare alla lettura della lista da un file JSON.
+
+Bonus
+Mostrare lo stato del task → se completato, barrare il testo
+Permettere di segnare un task come completato facendo click sul testo
+Permettere il toggle del task (completato/non completato)
+Abilitare l’eliminazione di un task
+*/
+/*
+milestone 1
+creare l'estetica della pagina  html con vue e css 
+collegare i file e verificare il collegamento tramite invio con input GET/POST
+
+milestone2
+collegare i dati ad un file data.json
+*/
+const {createApp} = Vue;
+const app = createApp({
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        
+    },
+    created() {
+      
+    },
+});
+app.mount('#app');
